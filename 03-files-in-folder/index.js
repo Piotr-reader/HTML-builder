77134aec5bff7e13db0e7aab673943f70a9399a3
Piotr-reader <piotr_partsianko@inbox.ru>
2022-05-15 23:23:56 +0300

    feat: implement task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+var path = require("path");
+const testFolder = "./03-files-in-folder/secret-folder/";
+
+fs.readdir(testFolder, (err, files) => {
+  if (err) console.log(err);
+  files.forEach((file) => {
+    fs.stat(`${testFolder}/${file}`, (err, files) => {
+      if (err) console.log(err);
+      if (files.isFile()) {
+        const fileName = path.parse(file).name;
+        const fileExtention = path.extname(file).slice(1);
+        const fileSize = (files.size / 1024).toFixed(3) + "kb";
+        console.log(fileName, "-", fileExtention, "-", fileSize);
+      }
+    });
+  });
+});
